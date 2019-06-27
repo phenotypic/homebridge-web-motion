33d3da65c0eba80c65c9357256b7f9569dd333f7
phenotypic <33377034+phenotypic@users.noreply.github.com>
2019-06-28 00:39:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Service, Characteristic
 const packageJson = require('./package.json')
+const ip = require('ip')
+const http = require('http')
 
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service
@@ -11,8 +13,10 @@ function WebMotion (log, config) {
   this.log = log
 
   this.name = config.name
-  this.apiroute = config.apiroute
-  this.pollInterval = config.pollInterval || 60
+  this.port = config.port || 2000
+
+  this.autoReset = config.autoReset || false
+  this.autoResetDelay = config.autoResetDelay || 5
 
   this.manufacturer = config.manufacturer || packageJson.author.name
   this.serial = config.serial || packageJson.version
@@ -24,12 +28,32 @@ function WebMotion (log, config) {
   this.timeout = config.timeout || 3000
   this.http_method = config.http_method || 'GET'
 
+  this.requestArray = ['motionDetected']
+
   if (this.username != null && this.password != null) {
     this.auth = {
       user: this.username,
       pass: this.password
     }
   }
+
+  this.server = http.createServer(function (request, response) {
+    var parts = request.url.split('/')
+    var partOne = parts[parts.length - 2]
+    var partTwo = parts[parts.length - 1]
+    if (parts.length === 3 && this.requestArray.includes(partOne) && partTwo.length === 1) {
+      this.log('Handling request: %s', request.url)
+      response.end('Handling request')
+      this._httpHandler(partOne, partTwo)
+    } else {
+      this.log.warn('Invalid request: %s', request.url)
+      response.end('Invalid request')
+    }
+  }.bind(this))
+
+  this.server.listen(this.port, function () {
+    this.log('Listen server: http://%s:%s', ip.address(), this.port)
+  }.bind(this))
 
   this.service = new Service.MotionSensor(this.name)
 }
@@ -41,7 +65,31 @@ WebMotion.prototype = {
     callback()
   },
 
+  _httpHandler: function (characteristic, value) {
+    switch (characteristic) {
+      case 'motionDetected':
+        this.log('Updating %s to: %s', characteristic, value)
+        this.service.getCharacteristic(Characteristic.MotionDetected).updateValue(value)
+        if (parseInt(value) === 1 && this.autoReset) {
+          this.autoResetFunction()
+        }
+        break
+      default:
+        this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value)
+    }
+  },
+
+  autoResetFunction: function () {
+    this.log('Waiting %s seconds to autoreset', this.autoResetDelay)
+    setTimeout(() => {
+      this.service.getCharacteristic(Characteristic.MotionDetected).updateValue(0)
+      this.log('Autoreset')
+    }, this.autoResetDelay * 1000)
+  },
+
   getServices: function () {
+    this.service.getCharacteristic(Characteristic.MotionDetected).updateValue(0)
+
     this.informationService = new Service.AccessoryInformation()
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
