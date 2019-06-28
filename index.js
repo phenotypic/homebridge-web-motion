4aba7ab35398b25c0d3d4e4be6b50c6d7244e86f
phenotypic <33377034+phenotypic@users.noreply.github.com>
2019-06-28 20:30:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,33 +6,31 @@ const http = require('http')
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service
   Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-web-motion', 'WebMotion', WebMotion)
+  homebridge.registerPlatform('homebridge-web-motion', 'WebMotion', WebMotion)
 }
 
 function WebMotion (log, config) {
   this.log = log
 
-  this.name = config.name
   this.port = config.port || 2000
-
   this.autoReset = config.autoReset || false
   this.autoResetDelay = config.autoResetDelay || 5
 
-  this.manufacturer = config.manufacturer || packageJson.author.name
-  this.serial = config.serial || packageJson.version
-  this.model = config.model || packageJson.name
-  this.firmware = config.firmware || packageJson.version
+  this.sensors = config.sensors
+  this.sensorAccessories = []
+  this.idArray = []
 
   this.requestArray = ['motionDetected']
 
   this.server = http.createServer(function (request, response) {
     var parts = request.url.split('/')
-    var partOne = parts[parts.length - 2]
-    var partTwo = parts[parts.length - 1]
-    if (parts.length === 3 && this.requestArray.includes(partOne) && partTwo.length === 1) {
+    var partOne = parts[parts.length - 3]
+    var partTwo = parts[parts.length - 2]
+    var partThree = parts[parts.length - 1]
+    if (parts.length === 4 && this.idArray.includes(partOne) && this.requestArray.includes(partTwo) && partThree.length === 1) {
       this.log('Handling request: %s', request.url)
       response.end('Handling request')
-      this._httpHandler(partOne, partTwo)
+      this._httpHandler(partOne, partTwo, partThree)
     } else {
       this.log.warn('Invalid request: %s', request.url)
       response.end('Invalid request')
@@ -42,40 +40,61 @@ function WebMotion (log, config) {
   this.server.listen(this.port, function () {
     this.log('Listen server: http://%s:%s', ip.address(), this.port)
   }.bind(this))
-
-  this.service = new Service.MotionSensor(this.name)
 }
 
 WebMotion.prototype = {
 
-  identify: function (callback) {
-    this.log('Identify requested!')
-    callback()
+  accessories: function (callback) {
+    var foundAccessories = []
+    var index
+    var count = this.sensors.length
+    for (index = 0; index < count; index++) {
+      var accessory = new MotionAccessory(this.sensors[index])
+      this.sensorAccessories[accessory.id] = accessory
+      this.idArray.push(accessory.id)
+      foundAccessories.push(accessory)
+    }
+    callback(foundAccessories)
   },
 
-  _httpHandler: function (characteristic, value) {
+  _httpHandler: function (id, characteristic, value) {
     switch (characteristic) {
       case 'motionDetected':
-        this.log('Updating %s to: %s', characteristic, value)
-        this.service.getCharacteristic(Characteristic.MotionDetected).updateValue(value)
+        this.log('%s | Updating %s to: %s', id, characteristic, value)
+        this.sensorAccessories[id].setState(value)
         if (parseInt(value) === 1 && this.autoReset) {
-          this.autoResetFunction()
+          this.autoResetFunction(id)
         }
         break
       default:
-        this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value)
+        this.log.warn('%s | Unknown characteristic "%s" with value "%s"', id, characteristic, value)
     }
   },
 
-  autoResetFunction: function () {
-    this.log('Waiting %s seconds to autoreset motion detection', this.autoResetDelay)
+  autoResetFunction: function (id) {
+    this.log('%s | Waiting %s seconds to autoreset motion detection', id, this.autoResetDelay)
     setTimeout(() => {
-      this.service.getCharacteristic(Characteristic.MotionDetected).updateValue(0)
-      this.log('Autoreset motion detection')
+      this.sensorAccessories[id].setState(0)
+      this.log('%s | Autoreset motion detection', id)
     }, this.autoResetDelay * 1000)
-  },
+  }
+
+}
+
+function MotionAccessory (config) {
+  this.name = config.name
+  this.id = config.id
+
+  this.manufacturer = config.manufacturer || packageJson.author.name
+  this.serial = config.serial || config.id
+  this.model = config.model || packageJson.name
+  this.firmware = config.firmware || packageJson.version
+}
+
+MotionAccessory.prototype = {
 
   getServices: function () {
+    this.service = new Service.MotionSensor(this.name)
     this.service.getCharacteristic(Characteristic.MotionDetected).updateValue(0)
 
     this.informationService = new Service.AccessoryInformation()
@@ -86,5 +105,10 @@ WebMotion.prototype = {
       .setCharacteristic(Characteristic.FirmwareRevision, this.firmware)
 
     return [this.informationService, this.service]
+  },
+
+  setState: function (value) {
+    this.service.getCharacteristic(Characteristic.MotionDetected).updateValue(value)
   }
+
 }
